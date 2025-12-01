ef13501e5e616af106c0af2aa137b30d3319a316
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-01 15:55:21 +0500

    First code in Javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+alert(
+  "Be Careful of using Document.write to print on screen, as this will lead to removing all the content from html"
+);
+console.log("My first code in javascript");
+var firstName = "Huzaifa";
+var lastName = "Khan";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var fullName = lastName + " " +firstName;
+console.log(fullName);
