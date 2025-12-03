261daf6b2fc2a7332e9d7b2352f7c0e41d442d77
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-03 21:15:13 +0500

    Logic Refined

--- Day 2 If Statements/main.js
+++ Day 2 If Statements/main.js
@@ -8,8 +8,8 @@ if(ageInput <=10){
     console.log("Congrats!🎉, Your Ticket🎫 is Free lil Kid");
 }
 
-if(ageInput >=10 <=20){
-    console.log("You are Eligible for Inclusive Discount👍 since you are" + ageInput + "Years old");
+if(ageInput >=10 && ageInput <=20){
+    console.log("You are Eligible for Inclusive Discount👍 since you are" + " " + ageInput + "Years old");
 }
 
 if(ageInput >60){
@@ -20,6 +20,10 @@ if(isMember == 'yes'){
     console.log("Welcome Back!🤚," + nameInput);
 }
 
-if(ageInput !== 10 >=20 <=10 !== 60 <60 !== isMember !== 'yes'){
+if(ageInput >20 && ageInput <60 && isMember == 'no' ){
     console.log("Unfortunately😐, Mr" + " " + nameInput + " " + "You are uneligible for any of our offers. Since you don't meet the following: \n1. 10 Years old or younger. \n2. 60 or Older. \n3. Must be a VIP member. \n4. Age b / w 10 - 20. \nBut Don't worry, We've got you covered. \nBecome a 👑VIP👑 Member & get 🫴50% Discount for getting 1st Time Membership🙂.");
+}
+
+if(ageInput >20 && ageInput <60 && isMember == '' ){
+    console.log("Welcome Back!, Mr" + " " + nameInput,+ "," + " " + "Long time no see!. We have some special offers for you.\n.1 Your next month subscription is Free😊\n.2 3rd Month's Subscription will be deducted to 🫴50%\n.3 You will be provided with the VIP ⊕ Badge for a month\nKeep Coming Back for Special Deals and Offers👍" );
 }
