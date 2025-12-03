b266ca5cac0be98eada5a061812bbec2eafccd48
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-03 22:32:27 +0500

    Logic is Improved for Ages

--- Day 2 If Statements/main.js
+++ Day 2 If Statements/main.js
@@ -8,25 +8,32 @@ if(ageInput >0 && ageInput <=10 && isMember == 'no'){
     console.log("Congrats!🎉, Your Ticket🎫 is Free lil Kid");
 }
 if(ageInput >0 && ageInput <=10 && isMember == 'yes'){
-    console.log("You are Welcome!, Mr. " + nameInput + ", Your Ticket🎫 is Free & Since you are our one of the Youngest Members, You will be provided with Extra Discount 75%,\nKeep Visiting for more values👍");
+    console.log("You are Welcome!, Mr. " + nameInput + ", Your Ticket🎫 is Free & Since you are our one of the Youngest Members, You will be provided with Extra Discount of 75%,\nKeep Visiting for more values👍");
 }
 
-if(ageInput >=10 && ageInput <=20){
+if(ageInput >=10 && ageInput <=20 && isMember == 'no'){
     console.log("You are Eligible for Inclusive Discount👍 since you are" + " " + ageInput + " " + "Years old");
 }
 
-if(ageInput >60){
-    console.log("Congratulations!🎉, Mr." + nameInput + "Your Ticket is 💯% Free  Old Man 😊");
+if(ageInput >=10 && ageInput <=20 && isMember == 'yes'){
+    console.log("You are Welcome once Again!, Young Boy🫡.\nYou are back at the right time⌛.Our Offer is about to end withing an Hour. Luckily🤞, You are added⊕ to the whishlist of 'First 100 Members Visit during Offer period' for this offer.");
 }
 
+if(ageInput >=60 && isMember == 'no'){
+    console.log("Congratulations!🎉, Mr." + nameInput + " Your Ticket is 💯% Free for the 1st Month. We respect old people 😊.");
+}
+
+if(ageInput >=60 && isMember == 'yes'){
+    console.log("Knock Knock!, Who's There🧓, " + nameInput + " Right after 30 Years, We are about to announce that 'Big Congratulation!🎉 to you sir, " + nameInput + " You've won the Free Forever 100% Offer🥇. Enjoy the site Forever👍. If you expect more, Contact us after 'Death☠' 🕶.");
+}
 // if(isMember == 'yes'){  
 //     console.log("Welcome Back!🤚," + nameInput);
 // }
 
 if(ageInput >20 && ageInput <60 && isMember == 'no' ){
-    console.log("Unfortunately😐, Mr" + " " + nameInput + ", You are uneligible for any of our offers. Since you don't meet the following: \n1. 10 Years old or younger. \n2. 60 or Older. \n3. Must be a VIP member. \n4. Age b / w 10 - 20. \nBut Don't worry, We've got you covered. \nBecome a 👑VIP👑 Member & get 🫴50% Discount for getting 1st Time Membership🙂.");
+    console.log("Unfortunately😐, Mr" + " " + nameInput + ", You are uneligible for any of our offers. Since you don't meet the following: \n1. 10 Years old or younger. \n2. 60 or Older. \n3. Must be a VIP member. \n4. Age b / w 10 - 20. \nBut Don't worry, We've got you covered. \nBecome a 👑VIP👑 Member & get 🫴50% Discount for being 1st Time Member🙂.");
 }
 
 if(ageInput >20 && ageInput <60 && isMember == 'yes' ){
-    console.log("Welcome Back!, Mr" + " " + nameInput + "," + " " + "Long time no see!. We have some special offers for you.\n.1 Your next month subscription is Free😊\n.2 3rd Month's Subscription will be deducted to 🫴50%\n.3 You will be provided with the VIP ⊕ Badge for a month.\nKeep Coming Back for Special Deals and Offers👍");
+    console.log("Welcome Back!, Mr" + " " + nameInput + "," + " " + "Long time no see!. We have some special offers for you.\n.1 Your next month subscription is Free😊\n.2 3rd Month's Subscription will be deducted to 🫴50%\n.3 You will be provided with the VIP ⊕ Badge for a month.\nKeep Coming Back for Special Deals and Offers👍.");
 }
