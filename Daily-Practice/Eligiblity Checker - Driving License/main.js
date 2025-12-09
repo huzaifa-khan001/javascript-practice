3880756e3275e0ab38e2395c2f12c5e15e454fcc
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-09 16:33:28 +0500

    Logic Refined

--- Daily-Practice/Eligiblity Checker - Driving License/main.js
+++ Daily-Practice/Eligiblity Checker - Driving License/main.js
@@ -1,23 +1,21 @@
-let age = prompt("Your age?");
-
-    age = Number(prompt);
-
 let hasLearnersPermit = true;
 
-let passedVisionTest = true;
+let passedVisionTest = false;
 
-if (hasLearnersPermit == true) {
-  if (age === 0 || age === -0 || age === "" || age === false || age === 0n) {
-    console.log("Age is Invalid!");
-  } else if (passedVisionTest == false) {
-    console.log("Need to pass the vision test first!");
-  } 
-  /* else if (age === "" || age <= 0) {
-console.log("Age is Required!");
+if (hasLearnersPermit == false) {
+  console.log("Must get a learner's permit first.");
+} else if (passedVisionTest == false) {
+  console.log("Must pass the vision test for the further checks");
+} else if (hasLearnersPermit == true) {
+  let age = prompt("Your age?");
+
+  // age = Number(prompt);
+
+  if (age === "" || age <= 0) {
+    console.log("Age is Required!");
   } else if (isNaN(age)) {
     console.log("Age must be a number!");
-  }*/
-    else if (age < 16 && age >0) {
+  } else if (age > 0 && age < 16) {
     console.log("Not eligible for any License, You are underage.");
   } else if (passedVisionTest == true && age >= 16 && age <= 17) {
     console.log("You can apply for the Standard License only.");
@@ -26,8 +24,8 @@ console.log("Age is Required!");
       "You are eligible for the Driving License & can apply for the Car or Motorcycle."
     );
   } else if (age > 80) {
-    console.log("Sorry sir, you are too old for the license!, you are uneligible for the Driving License. And this is for your safety!");
+    console.log(
+      "Sorry sir, you don't meet the age criteria (Must be younger than 80) for the the Driving License!, this is for your safety!"
+    );
   }
-} else {
-  console.log("Must get a learner's permit first.");
 }
