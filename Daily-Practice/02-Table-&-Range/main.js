e74f9d97846fffbce80cb518dc27f1b48167803c
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-13 22:35:18 +0500

    Input Validation

--- Daily-Practice/02-Table-&-Range/main.js
+++ Daily-Practice/02-Table-&-Range/main.js
@@ -2,6 +2,18 @@ let table = prompt("Which Table");
 
 let range = prompt("What's the range");
 
-for(var i = 1; i <= range; i++){
+if(isNaN(table) || isNaN(range)){
+    console.log("Can only be a Number!");
+}
+
+else if(table == "" || range == ""){
+    console.log("Cannot be empty!");
+}
+
+else if(!table || !range){
+    console.log("invalid Input!");
+}
+
+else for(var i = 1; i <= range; i++){
     console.log(table + " x", i, "=" ,i * table);
 }
