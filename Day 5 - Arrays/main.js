8034226a63d883088d54080ab6371486db1f3038
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-14 22:57:46 +0500

    Practicing Add - Remove Elementes

---
+++ Day 5 - Arrays/main.js
@@ -0,0 +1,9 @@
+let array1 = ["atlanta", "baltimore", "denver", "chicago", "washington", "log angeles", "log-vegas"];
+
+let array1Length = array1.length;
+
+array1.pop();
+
+array1.push("Karachi");
+
+console.log(array1);
