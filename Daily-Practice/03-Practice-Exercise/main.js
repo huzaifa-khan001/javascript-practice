0644da92419f3f052bfd3afbcebbdd455266ae4f
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-15 23:28:10 +0500

    Check Learned Concepts

--- Daily-Practice/03-Practice-Exercise/main.js
+++ Daily-Practice/03-Practice-Exercise/main.js
@@ -19,3 +19,12 @@ newArray = cities.slice(2,4);
 newArray.push("Helsinki");
 
 console.log(cities, newArray);
+
+console.log(hobby);
+// Says Undefined & Merely Keeps Errors that is Risky
+var hobby = "Car Drifting";
+
+console.log(passion);
+// This is the Tempral Dead Zone for Let
+let passion = "Full-Stack Web Developer";
+
