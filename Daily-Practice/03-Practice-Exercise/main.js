df83ba4483ec5769aa11b1826f8ec47d5d19dafe
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-15 23:20:38 +0500

    Array Practice

--- Daily-Practice/03-Practice-Exercise/main.js
+++ Daily-Practice/03-Practice-Exercise/main.js
@@ -7,3 +7,15 @@ const profile = {
 profile.firstName = "Zaid";
 
 console.log(profile);
+
+const cities = ["Atlanta", "Georgia", "Las Vegas", "baltimore", "denver", "tokyo", "Helsinki"];
+
+cities.pop();
+
+citiesLength = cities.length -3;
+
+newArray = cities.slice(2,4);
+
+newArray.push("Helsinki");
+
+console.log(cities, newArray);
