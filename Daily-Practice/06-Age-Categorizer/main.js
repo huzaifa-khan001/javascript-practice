33f89b1602886b54ce96c4727a7c3492271323c4
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-17 23:54:47 +0500

    Input Validation

--- Daily-Practice/06-Age-Categorizer/main.js
+++ Daily-Practice/06-Age-Categorizer/main.js
@@ -1,51 +1,58 @@
 // Ask how many people
 var count = prompt("How many people?");
-
-// Convert to number
 count = Number(count);
 
-// Array to store ages
-var ages = [];
+// Basic validation for count
+if (isNaN(count) || count <= 0) {
+  alert("Invalid number of people. Program will stop.");
+} else {
 
-// Take ages from user
-for (var i = 0; i < count; i++) {
-  var age = prompt("Enter age of person " + (i + 1));
-  age = Number(age);
-  ages.push(age);
-}
+  var ages = [];
 
-// Counters
-var childCount = 0;
-var teenCount = 0;
-var adultCount = 0;
-var seniorCount = 0;
+  // Take ages from user
+  for (var i = 0; i < count; i++) {
+    var age = prompt("Enter age of person " + (i + 1));
+    age = Number(age);
 
-// Categorize ages
-for (var j = 0; j < ages.length; j++) {
-  var currentAge = ages[j];
-
-  if (currentAge >= 0 && currentAge <= 12) {
-    alert("Person " + (j + 1) + ": Child");
-    childCount++;
-  } else if (currentAge >= 13 && currentAge <= 19) {
-    alert("Person " + (j + 1) + ": Teen");
-    teenCount++;
-  } else if (currentAge >= 20 && currentAge <= 59) {
-    alert("Person " + (j + 1) + ": Adult");
-    adultCount++;
-  } else if (currentAge >= 60) {
-    alert("Person " + (j + 1) + ": Senior");
-    seniorCount++;
-  } else {
-    alert("Person " + (j + 1) + ": Invalid age");
+    // Validate age
+    if (isNaN(age) || age < 0 || age > 120) {
+      alert("Invalid age entered. This person will be skipped.");
+    } else {
+      ages.push(age);
+    }
   }
-}
 
-// Summary
-alert(
-  "Summary:\n" +
-  "Children: " + childCount + "\n" +
-  "Teens: " + teenCount + "\n" +
-  "Adults: " + adultCount + "\n" +
-  "Seniors: " + seniorCount
-);
+  // Counters
+  var childCount = 0;
+  var teenCount = 0;
+  var adultCount = 0;
+  var seniorCount = 0;
+
+  // Categorize valid ages
+  for (var j = 0; j < ages.length; j++) {
+    var currentAge = ages[j];
+
+    if (currentAge <= 12) {
+      childCount++;
+      alert("Person " + (j + 1) + ": Child");
+    } else if (currentAge <= 19) {
+      teenCount++;
+      alert("Person " + (j + 1) + ": Teen");
+    } else if (currentAge <= 59) {
+      adultCount++;
+      alert("Person " + (j + 1) + ": Adult");
+    } else {
+      seniorCount++;
+      alert("Person " + (j + 1) + ": Senior");
+    }
+  }
+
+  // Summary
+  alert(
+    "Summary:\n" +
+    "Children: " + childCount + "\n" +
+    "Teens: " + teenCount + "\n" +
+    "Adults: " + adultCount + "\n" +
+    "Seniors: " + seniorCount
+  );
+}
