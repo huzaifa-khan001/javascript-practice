730520bb946066086e36b4425c3075f5e02239f8
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-03 22:56:58 +0500

    Logic Improved

--- Daily-Practice/09-Smaller-Value-Taker/main.js
+++ Daily-Practice/09-Smaller-Value-Taker/main.js
@@ -1,21 +1,19 @@
 var range = prompt("How many values in an Array");
 
-var userInput;
-
 var arr = [];
 
-for(var i = 1; i <= range; i++){
-    userInput = prompt("Value " + i);
-    userInput = Number(userInput);
-    arr.push(userInput);
+for(var i =0; i <range; i++){
+    arr[i] = Number(prompt("Value " + i));
 }
 
 var smallest = arr[0];
 
-for(var j=0; j < arr.length; j++){
+for(var j=1; j < arr.length; j++){
     if(smallest > arr[j]){
         smallest = arr[j]
     }
+    smallest = Number(smallest);
 }
 
+console.log(arr);
 console.log(smallest);
