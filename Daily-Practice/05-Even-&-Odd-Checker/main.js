e29d0407265a34cead32367d62dc12ef1302072d
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-03 15:45:28 +0500

    Even & Odd Counter Added

--- Daily-Practice/05-Even-&-Odd-Checker/main.js
+++ Daily-Practice/05-Even-&-Odd-Checker/main.js
@@ -1,4 +1,6 @@
 var nums = [];
+var evenCounter = 0;
+var oddCounter = 0;
 
 for (var i = 1; i <= 5; i++){
    var userInput = prompt("Enter Number " + i);
@@ -12,8 +14,13 @@ for (var i = 1; i <= 5; i++){
 for(var i = 0; i < nums.length; i++){
     if(nums[i] % 2){
         console.log(nums[i], " Is Odd Number");
+        oddCounter++;
     }
     else{
         console.log(nums[i], " Is Even Number");
+        evenCounter++;
     }
 }
+
+console.log("Even Count:" + evenCounter);
+console.log("Odd Count:" + oddCounter);
