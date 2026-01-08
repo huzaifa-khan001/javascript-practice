13fbfa94a6b0aa944fbdc06dedaea6d57181a727
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-08 23:36:22 +0500

    Text Length Checker - Function

--- Daily-Practice/12-Function-Practice/main.js
+++ Daily-Practice/12-Function-Practice/main.js
@@ -1,11 +1,25 @@
-var nameFromUser = prompt("Enter your name");
+// var nameFromUser = prompt("Enter your name");
 
-function welcomeUser(userName) {
-    var lowerName = userName.toLowerCase();
-    var message = "welcome, " + lowerName;
-    return message;
+// function welcomeUser(userName) {
+//     var lowerName = userName.toLowerCase();
+//     var message = "welcome, " + lowerName;
+//     return message;
+// }
+
+// var result = welcomeUser(nameFromUser);
+
+// console.log(result);
+
+function checkString(text) {
+    var lowerText = text.toLowerCase();
+
+    if (lowerText.length < 5) {
+        return "too short";
+    } else {
+        return "accepted";
+    }
 }
 
-var result = welcomeUser(nameFromUser);
+console.log(checkString("Ali"));
+console.log(checkString("Ahmed"));
 
-console.log(result);
