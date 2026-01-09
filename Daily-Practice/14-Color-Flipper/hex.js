f326326f2c14362ba7c71287e0988ebf0df0f19d
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-10 02:39:01 +0500

    Hex Color Generator - Completed

---
+++ Daily-Practice/14-Color-Flipper/hex.js
@@ -0,0 +1,21 @@
+let hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+let hex_btn = document.getElementById("hex-btn");
+
+let hex_color = document.getElementsByClassName("hex-color")[0];
+
+hex_btn.addEventListener("click", function () {
+  let hexColor = "#";
+
+  for (var i = 0; i < 6; i++) {
+    hexColor += hex[randomColor()];
+  }
+  
+  document.getElementsByTagName("main")[0].style.backgroundColor = hexColor;
+  hex_color.innerHTML = hexColor;
+});
+
+function randomColor() {
+  let randomColor = Math.floor(Math.random() * hex.length);
+  return randomColor;
+}
--- Daily-Practice/14-Color-Flipper/main.js
+++ Daily-Practice/14-Color-Flipper/main.js
@@ -1,14 +1,13 @@
 let colors = ["red", "green", "blue", "rgba(255, 0, 200, 1)"];
 
-let btn = document.getElementById('btn');
+let btn = document.getElementById("btn");
 
-let color = document.getElementsByClassName('color')[0];
+let color = document.getElementsByClassName("color")[0];
 
-let bgColor = document.getElementsByTagName('main')[0];
+let bgColor = document.getElementsByTagName("main")[0];
 
-
-btn.addEventListener("click", function(){
-    let randomColor = Math.floor(Math.random() * colors.length);
-    bgColor.style.backgroundColor = colors[randomColor];
-    color.innerHTML = colors[randomColor];
-})
+btn.addEventListener("click", function () {
+  let randomColor = Math.floor(Math.random() * colors.length);
+  bgColor.style.backgroundColor = colors[randomColor];
+  color.innerHTML = colors[randomColor];
+});
