f9888e2007f260a0586a9ba7afc1005ca70067d6
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-11 00:00:04 +0500

    Validation

--- Daily-Practice/15-Percentage-Calculator/main.js
+++ Daily-Practice/15-Percentage-Calculator/main.js
@@ -1,9 +1,13 @@
-
-
 function calcPer(){
-    let totalMarks = document.getElementById('total-marks').value;
-    let obtMarks = document.getElementById('obtained-marks').value;
-    
-    let percentage = "PERCENTAGE : " + (obtMarks / totalMarks) * 100+"%";
-    document.getElementsByClassName('display-percentage')[0].innerText = percentage;
+    let totalMarks = Number(document.getElementById('total-marks').value);
+    let obtMarks = Number(document.getElementById('obtained-marks').value);
+
+    if(obtMarks > totalMarks || obtMarks < 0){
+        alert("Obtained Marks must be between 1 to " +totalMarks +" & less than Total Marks")
+        return;
+    }
+    let percentage = (obtMarks / totalMarks) * 100;
+    percentage.toFixed(2);
+    document.getElementsByClassName('display-percentage')[0].innerText = "PERCENTAGE : " +percentage+"%";
+
 }
