2fd117e336f41b31ac1697aa6e5e9c43e2db9e8a
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-11 00:14:11 +0500

    Further Validation

--- Daily-Practice/15-Percentage-Calculator/main.js
+++ Daily-Practice/15-Percentage-Calculator/main.js
@@ -1,13 +1,21 @@
-function calcPer(){
-    let totalMarks = Number(document.getElementById('total-marks').value);
-    let obtMarks = Number(document.getElementById('obtained-marks').value);
+function calcPer() {
+  let totalMarks = Number(document.getElementById("total-marks").value);
+  let obtMarks = Number(document.getElementById("obtained-marks").value);
 
-    if(obtMarks > totalMarks || obtMarks < 0){
-        alert("Obtained Marks must be between 1 to " +totalMarks +" & less than Total Marks")
-        return;
-    }
-    let percentage = (obtMarks / totalMarks) * 100;
-    percentage.toFixed(2);
-    document.getElementsByClassName('display-percentage')[0].innerText = "PERCENTAGE : " +percentage+"%";
+  if (totalMarks === 0 || obtMarks === 0) {
+    alert("Cannot be empty!");
+    return;
+  }
+  if (obtMarks > totalMarks || obtMarks < 0) {
+    alert(
+      "Obtained Marks must be between 1 to " +
+        totalMarks +
+        " & less than Total Marks"
+    );
+    return;
+  }
 
+  let percentage = (obtMarks / totalMarks) * 100;
+  document.getElementsByClassName("display-percentage")[0].innerText =
+    "PERCENTAGE : " + percentage.toFixed(2) + "%";
 }
