520cd826682b132d7dd3573db7aec0c9050bc06d
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-20 03:11:18 +0500

    fix: reasonable folder names

---
+++ Daily-Practice/A-codingJS-Exercises/07-Near-Hundred/main.js
@@ -0,0 +1,5 @@
+function nearHundred(n){
+  n = Math.abs(n);
+  return n >= 90 && n <= 110 || n >= 190 && n <= 210
+}
+// Math.abs(value) Makes any value absolute(positive) even if provided negative
