3c7de92be727936a06d783392f9e2113f6ad8d77
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-29 00:47:20 +0500

    feat: stop the counter on completion

--- Daily-Practice/23-Promodoro-Timer/main.js
+++ Daily-Practice/23-Promodoro-Timer/main.js
@@ -1,21 +1,34 @@
 let minutesDisplay = document.getElementById('minutes');
 let secondsDisplay = document.getElementById('seconds');
-
+let dot = document.getElementById('dot');
+let interval;
 let minutes = 0;
 let seconds = 5;
 
 function start(){
 
+    if(minutesDisplay.innerText === "Great Job!"){
+    clearInterval(interval);
+    return
+}
+
     if(seconds === 1){
         minutes = minutes-1;
         seconds = 60;
     }
+
     seconds = seconds -1;
 
+        if(minutes === 0 && seconds === 1){
+        minutesDisplay.innerText = "Great Job!";
+        secondsDisplay.innerText = "🥳"
+        dot.innerText = '';
+        return
+    }
+    
     minutesDisplay.innerText = minutes;
     secondsDisplay.innerText = seconds;
-    
+
 }
 
-setInterval(start, 1000);
-
+   interval = setInterval(start, 1000);
