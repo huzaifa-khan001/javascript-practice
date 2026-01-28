5ff9ec74bd5e8c95ef6e9dbc8025a6f4e48fb31e
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-28 22:41:32 +0500

    feat: declare variables for timer

---
+++ Daily-Practice/23-Promodoro-Timer/main.js
@@ -0,0 +1,8 @@
+let timer = document.getElementById('timer');
+let time = new Date(2026, 0, 0, 0, 25, 0, 0);
+let minutes = time.getMinutes();
+let seconds = time.getSeconds();
+
+function start(){
+
+}
