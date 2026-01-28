ca644cc07139c4f44164131b7ce9715c2e680b4d
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-28 23:57:53 +0500

    feat: partially complete logic for start button

--- Daily-Practice/23-Promodoro-Timer/main.js
+++ Daily-Practice/23-Promodoro-Timer/main.js
@@ -1,8 +1,11 @@
-let timer = document.getElementById('timer');
+let minutesDisplay = document.getElementById('minutes');
+let secondsDisplay = document.getElementById('seconds');
 let time = new Date(2026, 0, 0, 0, 25, 0, 0);
 let minutes = time.getMinutes();
 let seconds = time.getSeconds();
 
 function start(){
-
+    minutesDisplay.innerText = minutes;
+    secondsDisplay.innerText = seconds;
+    setInterval(start, 1000);
 }
