7f072f5b8895ac89576c31e666f2649a886fdd69
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-29 00:26:44 +0500

    refactor: make the countdown work

--- Daily-Practice/23-Promodoro-Timer/main.js
+++ Daily-Practice/23-Promodoro-Timer/main.js
@@ -1,11 +1,21 @@
 let minutesDisplay = document.getElementById('minutes');
 let secondsDisplay = document.getElementById('seconds');
-let time = new Date(2026, 0, 0, 0, 25, 0, 0);
-let minutes = time.getMinutes();
-let seconds = time.getSeconds();
+
+let minutes = 0;
+let seconds = 5;
 
 function start(){
+
+    if(seconds === 1){
+        minutes = minutes-1;
+        seconds = 60;
+    }
+    seconds = seconds -1;
+
     minutesDisplay.innerText = minutes;
     secondsDisplay.innerText = seconds;
-    setInterval(start, 1000);
+    
 }
+
+setInterval(start, 1000);
+
