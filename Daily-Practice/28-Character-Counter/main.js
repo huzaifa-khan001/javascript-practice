f4bd52fca98d2041cfef0cf8bfa2381f9ec5074b
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-02-14 01:39:05 +0500

    feat: display message upon reaching the character limit

--- Daily-Practice/28-Character-Counter/main.js
+++ Daily-Practice/28-Character-Counter/main.js
@@ -1,13 +1,19 @@
-let totalCharDisplay = document.getElementById('total-char');
-let remainingCharDisplay = document.getElementById('remaining-char');
-let charactersTyped = document.getElementById('text-area');
+let totalCharDisplay = document.getElementById("total-char");
+let remainingCharDisplay = document.getElementById("remaining-char");
+let charactersTyped = document.getElementById("text-area");
+let limitReached = document.getElementById("limit-reached");
 let totalChar = 50;
 
 remainingCharDisplay.innerText = totalChar;
 
-charactersTyped.addEventListener('keyup', characterCounter);
+charactersTyped.addEventListener("keyup", characterCounter);
 
-function characterCounter(){
-    totalCharDisplay.innerText = charactersTyped.value.length;
-    remainingCharDisplay.innerText = totalChar - charactersTyped.value.length;
+function characterCounter() {
+  totalCharDisplay.innerText = charactersTyped.value.length;
+  remainingCharDisplay.innerText = totalChar - charactersTyped.value.length;
+  if (Number(remainingCharDisplay.innerText) === 0) {
+    limitReached.style.display = "block";
+  } else {
+    limitReached.style.display = "none";
+  }
 }
